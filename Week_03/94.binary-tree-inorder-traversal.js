52b4a3ec4c3ef7680c6b6949aa53b47a5858b81a
daydaywen <dayday.wen@foxmail.com>
2020-09-15 10:46:32 +0800

    feat: add homeworks of week03

---
+++ Week_03/94.binary-tree-inorder-traversal.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (66.52%)
+ * Likes:    498
+ * Dislikes: 0
+ * Total Accepted:    159.2K
+ * Total Submissions: 222.8K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的中序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * 输出: [1,3,2]
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  // the end condition
+  if (!root) return [];
+
+  // 前序
+  const leftValArr = inorderTraversal(root.left);
+  // 中序
+  const rootValArr = [root.val];
+  const rightValArr = inorderTraversal(root.right);
+  // 后序
+  return [...leftValArr, ...rootValArr, ...rightValArr];
+};
+// @lc code=end
+
